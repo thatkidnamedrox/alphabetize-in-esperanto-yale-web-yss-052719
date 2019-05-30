f6873050763a7024d52b113041a21355370ef152
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 23:33:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,5 +3,5 @@ def alphabetize(arr)
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
 
-  arr.sort_by{|string| string.tr(esp_aplh, ascii)}
+  arr.sort_by{|string| string.tr(esp_alph, ascii)}
 end
